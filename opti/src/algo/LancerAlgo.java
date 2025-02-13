df7f904f4700cd8293d9dd21b882ed87ffe94060
Maelle26 <maelle.bitsindou2@etu.univ-lorraine.fr>
2025-02-13 17:26:41 +0100

    Ajout de l'algo glouton FIN

--- opti/src/algo/LancerAlgo.java
+++ opti/src/algo/LancerAlgo.java
@@ -1,5 +1,11 @@
 package algo;
 
+import outils.Besoin;
+import outils.Salarie;
+import outils.donnees.ImportCSV;
+
+import java.util.List;
+
 public class LancerAlgo {
 
     public void run() {
@@ -9,8 +15,15 @@ public class LancerAlgo {
     public static void main(String[] args) {
         AlgoGlouton algoGlouton = new AlgoGlouton();
 
+        //on prend un fichier CSV pour le transfome en liste de besoins et de salaries
+        ImportCSV importCSV = new ImportCSV("/home/maelle/Documents/crazy_charky_day-Team-Otes/opti/ressource/csv/00_exemple/metier_1.csv");
+        List<Besoin> listeBesoin = importCSV.getBesoin();
+        List <Salarie> listeSalaries = importCSV.getSalaries();
+
+
         System.out.println("Liste des affectations : ");
-        System.out.println(algoGlouton.lancerCalcul());
+        System.out.println(algoGlouton.lancerCalcul(listeBesoin, listeSalaries));
+        System.out.println("Score : " + algoGlouton.score);
     }
 
 }
