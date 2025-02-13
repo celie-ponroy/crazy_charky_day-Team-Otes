623faa7f27da8ca5ab2d5de9cd25164c7d386dd0
Maelle26 <maelle.bitsindou2@etu.univ-lorraine.fr>
2025-02-13 20:23:40 +0100

    fin glouton

--- opti/src/algo/LancerAlgo.java
+++ opti/src/algo/LancerAlgo.java
@@ -2,6 +2,7 @@ package algo;
 
 import outils.Besoin;
 import outils.Salarie;
+import outils.donnees.ExportCSV;
 import outils.donnees.ImportCSV;
 
 import java.util.List;
@@ -16,13 +17,15 @@ public class LancerAlgo {
         AlgoGlouton algoGlouton = new AlgoGlouton();
 
         //on prend un fichier CSV pour le transfome en liste de besoins et de salaries
-        ImportCSV importCSV = new ImportCSV("/home/maelle/Documents/crazy_charky_day-Team-Otes/opti/ressource/csv/00_exemple/metier_1.csv");
+        ImportCSV importCSV = new ImportCSV("/home/maelle/Documents/crazy_charky_day-Team-Otes/opti/ressource/csv/01_pb_simples/Probleme_2_nbSalaries_3_nbClients_3_nbTaches_5.csv");
         List<Besoin> listeBesoin = importCSV.getBesoin();
         List <Salarie> listeSalaries = importCSV.getSalaries();
 
+        ExportCSV ex = new ExportCSV();
 
         System.out.println("Liste des affectations : ");
         System.out.println(algoGlouton.lancerCalcul(listeBesoin, listeSalaries));
+        ex.exporterSolution(algoGlouton.lancerCalcul(listeBesoin, listeSalaries),algoGlouton.score,"/home/maelle/Documents/crazy_charky_day-Team-Otes/opti/ressource/csv/01_pb_simples/Probleme_1_nbSalaries_3_nbClients_3_nbTaches_2.csv");
         System.out.println("Score : " + algoGlouton.score);
     }
 
