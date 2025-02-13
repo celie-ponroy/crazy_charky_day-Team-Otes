95561cbff07ed859f72ef7fd3fe092701c0fc47a
gemo98 <utp0156528@alumno.utpuebla.edu.mx>
2025-02-13 15:43:20 +0100

    Enhance error handling and validation in PostSalarie and GetSalaries actions

--- backend/src/core/service/interfaces/UserServiceInterface.php
+++ backend/src/core/service/interfaces/UserServiceInterface.php
@@ -2,7 +2,10 @@
 
 namespace charly\core\service\interfaces;
 
+use charly\core\dto\input\InputCreateSalarie;
+
 interface UserServiceInterface
 {
-
+    public function createSalarie(InputCreateSalarie $inputCreateSalarie);
+    public function getSalaries ():array;
 }
--- backend/src/infrastructure/repository/interfaces/UserRepositoryInterface.php
+++ backend/src/infrastructure/repository/interfaces/UserRepositoryInterface.php
@@ -2,7 +2,12 @@
 
 namespace charly\infrastructure\repository\interfaces;
 
+use charly\core\domain\entity\User;
+use charly\core\dto\input\InputCreateSalarie;
+use charly\core\dto\UserDTO;
+
 interface UserRepositoryInterface
 {
-
+    public function createSalarie(InputCreateSalarie $inputCreateSalarie):User;
+    public function getSalaries():array;
 }
