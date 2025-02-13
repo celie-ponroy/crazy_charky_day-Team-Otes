f58075ad36cee76d6d4c49889ec6c8f85f947fdd
hirtz-gregoire <hirtz.gregoire@ghmail.fr>
2025-02-13 19:17:06 +0100

    maj api

---
+++ opti/src/outils/donnees/ExportBD.java
@@ -0,0 +1,15 @@
+package outils.donnees;
+
+import outils.Affectation;
+
+import java.util.List;
+
+public class ExportBD implements Export{
+
+    @Override
+    public void exporterSolution(List<Affectation> affactations,int score,String path) {
+
+    }
+}
+
+
